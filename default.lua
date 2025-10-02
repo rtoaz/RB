84afc144fe439c89ceeed064215c3b78965c0c79
BS58dL <17867876100@139.com>
2025-10-03 00:12:02 +0800

    Update default.lua

--- default.lua
+++ default.lua
@@ -1,1 +1,13 @@
+local DefaultMessages = {}
 
+DefaultMessages.Messages = {
+    "通用消息1",
+    "通用消息2",
+    "通用消息3"
+}
+
+function DefaultMessages.GetMessage()
+    return DefaultMessages.Messages[math.random(1, #DefaultMessages.Messages)]
+end
+
+return DefaultMessages
